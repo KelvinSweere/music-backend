7677516efbcda5018ed19e6c2196451d99a2acc6
Kelvin Sweere <kelvin.sweere@teamrockstars.nl>
2023-05-16 15:20:22 +0200

    Added initialization of the database, if tables don't exists.

--- src/infrastructure/databases/sql_alchemy.py
+++ src/infrastructure/databases/sql_alchemy.py
@@ -8,19 +8,18 @@ sqlalchemy_db = SQLAlchemy()
 class SQLAlchemyAdapter:
 
     def __init__(self, app):
-
         if app.config[SQLALCHEMY_DATABASE_URI] is not None:
             sqlalchemy_db.init_app(app)
+            with app.app_context():
+                sqlalchemy_db.create_all()
         elif not app.config["TESTING"]:
             raise OperationalException("SQLALCHEMY_DATABASE_URI not set")
 
 
 def setup_sqlalchemy(app, throw_exception_if_not_set=True):
-
     try:
         SQLAlchemyAdapter(app)
     except OperationalException as e:
         if throw_exception_if_not_set:
             raise e
-
     return app
