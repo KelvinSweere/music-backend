e428c8bfeb28ba7b855bd7cf3211fed25f959804
Kelvin Sweere <kelvin.sweere@teamrockstars.nl>
2023-05-17 15:54:25 +0200

    Added missing extra checks for creating an artist.

--- src/infrastructure/services/artist_context_service.py
+++ src/infrastructure/services/artist_context_service.py
@@ -1,15 +1,20 @@
 from src.infrastructure.models.artist_context import ArtistContext
 from src.infrastructure.databases import sqlalchemy_db as db
+from flask import abort
 
 class ArtistContextService:
 
 		def create_artist(self, data):
-			artist_context = ArtistContext(**data)
-
-			db.session.add(artist_context)
-			db.session.commit()
-			return artist_context
-
+			if self.get_artist_by_id(data['Id']) is not None:
+					return abort(409, 'A artist with the provided ID already exists.')
+			try:
+				artist_context = ArtistContext(**data)
+				db.session.add(artist_context)
+				db.session.commit()
+				return artist_context
+			except Exception as e:
+				return abort(400, e)
+		
 		def get_all_artists(self):
 			artist_context = ArtistContext.query.all()
 			return artist_context
